cb3dfcbed0161a4a92443ecab2242249d080e416
donkomura <koiru3822fs@gmail.com>
2019-10-04 00:56:22 +0900

    Update instance config: zone

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,7 +6,7 @@ provider "google" {
 resource "google_compute_instance" "vm_instance" {
   name         = "terraform-instance"
   machine_type = "f1-micro"
-  zone = "asia-northeast1-a"
+  zone = "asia-northeast1-b"  
 
   boot_disk {
     initialize_params {
